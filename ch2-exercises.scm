8e95ed7fe9ef5ea930888e67467f1cb31a22326e
Jacob Lambert <jacob.lambert@gmail.com>
2017-03-02 18:25:29 +0800

    Complete interval multiplication refactoring exercise, a little long!

--- ch2-exercises.scm
+++ ch2-exercises.scm
@@ -213,7 +213,60 @@
 ; The widths of first and second are equal, but the widths of the products and quotients are different
 ; Thus, the width of a product or quotient interval is not a function of the widths of the factors or divisors
 
-; 2.10
+; 2.10 Division with an interval spanning zero
+
+(define (div-interval x y)
+  (if (spans-zero? y)
+      (error "INVALID OPERATION: Division by a zero-spanning interval! div-interval")
+      (mul-interval x
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y)))))
+
+(define (spans-zero? x)
+  (and (< (lower-bound x) 0) (> (upper-bound x) 0)))
+
+; 2.11 Refactoring mul-interval
+
+(define (pos? x)
+  (and (> (lower-bound x) 0) (> (upper-bound x) 0)))
+
+(define (neg? x)
+  (and (< (lower-bound x) 0) (< (upper-bound x) 0)))
+
+; The point of this refactored mul-interval is to rely on conditionals to split the multiplication up into 9 cases
+; Most of the cases only involve two multiplications, as opposed to the original mul-interval's four
+; However, if both intervals span zero, the four multiplications are still necessary
+; Along with the conditionals, this makes the operation a bit slower for zero-spanning intervals
+; But, if multiplication is more resource-expensive than the pos? and neg? predicates,
+; this mul-interval should be much more performant
+
+(define (mul-interval x y)
+  (let ((x-lo (lower-bound x))
+        (x-hi (upper-bound x))
+        (y-lo (lower-bound y))
+        (y-hi (upper-bound y)))
+       (cond ((pos? x)
+                (cond ((pos? y) (make-interval (* x-lo y-lo) (* x-hi y-hi)))
+                      ((neg? y) (make-interval (* x-hi y-lo) (* x-lo y-hi)))
+                      (else     (make-interval (* x-hi y-lo) (* x-hi y-hi)))))
+             ((neg? x)
+                (cond ((pos? y) (make-interval (* x-lo y-hi) (* x-hi y-lo)))
+                      ((neg? y) (make-interval (* x-hi y-hi) (* x-lo y-lo)))
+                      (else     (make-interval (* x-lo y-hi) (* x-lo y-lo)))))
+             (else
+                (cond ((pos? y) (make-interval (* x-lo y-hi) (* x-hi y-hi)))
+                      ((neg? y) (make-interval (* x-hi y-lo) (* x-lo y-lo)))
+                      (else    (orig-mul-interval x y)))))))
+
+(define (orig-mul-interval x y)
+   (let ((p1 (* (lower-bound x) (lower-bound y)))
+         (p2 (* (lower-bound x) (upper-bound y)))
+         (p3 (* (upper-bound x) (lower-bound y)))
+         (p4 (* (upper-bound x) (upper-bound y))))
+        (make-interval (min p1 p2 p3 p4)
+                       (max p1 p2 p3 p4))))
+
+
 
 (define (make-center-width c w)
    (make-interval (- c w) (+ c w)))
